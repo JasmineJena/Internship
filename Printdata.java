17ed677c3004dcc316ef145aaab952ebbac418da
JasmineJena <48236196+JasmineJena@users.noreply.github.com>
2021-04-05 09:23:33 +0545

    Add files via upload

---
+++ Printdata.java
@@ -0,0 +1,10 @@
+public class Printdata{
+    public static void main(String[] args){
+        int a = 26;
+        double b = 33.12;
+        char c = 's';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
